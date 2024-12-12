a3307afd59993fa2e65441624f3721756f3320dd
Saulius <sauliusenator@gmail.com>
2024-12-12 19:17:15 +0200

    45

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import User from "./user.js";
 import { printName, printAge } from "./user.js";
 
-const user = new User("Bob", 35);
+const user = new User("Bob", 45);
 
 printName(user);
 printAge(user);
